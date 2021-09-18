eabddd8403f369bd284fa2c947fa703020a76234
santiagojgonzalez <santiagojgonzalezfernandez@gmail.com>
2021-09-18 17:47:50 +0200

    Se aumenta el nivel de PHPStan al 8 y se tipan los Test

--- tests/GildedRoseTest.php
+++ tests/GildedRoseTest.php
@@ -11,106 +11,109 @@ class GildedRoseTest extends TestCase
     /**
      * @test
      */
-    public function itemsDegradeQuality()
+    public function itemsDegradeQuality(): void
     {
         $items = [new Item('', 1, 5)];
 
-  		$gilded_rose = new GildedRose($items);
+        $gilded_rose = new GildedRose($items);
         $gilded_rose->update_quality();
 
         $this->assertEquals(4, $items[0]->quality);
-  	}
+    }
 
     /**
      * @test
      */
-    public function itemsDegradeDoubleQualityOnceTheSellInDateHasPass()
+    public function itemsDegradeDoubleQualityOnceTheSellInDateHasPass(): void
     {
-  		$items = [new Item('', -1, 5)];
-
-  		$gilded_rose = new GildedRose($items);
-        $gilded_rose->update_quality();
-
-  		$this->assertEquals(3, $items[0]->quality);
-  	}
-
-    /**
-     * @test
-     */
-    public function itemsCannotHaveNegativeQuality()
-    {
-  		$items = [new Item('', 0, 0)];
-
-  		$gilded_rose = new GildedRose($items);
-        $gilded_rose->update_quality();
-
-  		$this->assertEquals(0, $items[0]->quality);
-  	}
-
-    /**
-     * @test
-     */
-    public function agedBrieIncreasesQualityOverTime()
-    {
-  		$items = [new Item('Aged Brie', 0, 5)];
+        $items = [new Item('', -1, 5)];
 
         $gilded_rose = new GildedRose($items);
         $gilded_rose->update_quality();
 
-  		$this->assertEquals(7, $items[0]->quality);
-  	}
+        $this->assertEquals(3, $items[0]->quality);
+    }
 
     /**
      * @test
      */
-    public function qualityCannotBeGreaterThan50()
+    public function itemsCannotHaveNegativeQuality(): void
     {
-  		$items = [new Item('Aged Brie', 0, 50)];
+        $items = [new Item('', 0, 0)];
 
         $gilded_rose = new GildedRose($items);
         $gilded_rose->update_quality();
 
-  		$this->assertEquals(50, $items[0]->quality);
-  	}
+        $this->assertEquals(0, $items[0]->quality);
+    }
 
     /**
      * @test
      */
-    public function sulfurasDoesNotChange()
+    public function agedBrieIncreasesQualityOverTime(): void
     {
-  		$items = [new Item('Sulfuras, Hand of Ragnaros', 10, 10)];
+        $items = [new Item('Aged Brie', 0, 5)];
 
         $gilded_rose = new GildedRose($items);
         $gilded_rose->update_quality();
 
-  		$this->assertEquals(10, $items[0]->sell_in);
-  		$this->assertEquals(10, $items[0]->quality);
-  	}
+        $this->assertEquals(7, $items[0]->quality);
+    }
 
-    public static function backstageRules()
+    /**
+     * @test
+     */
+    public function qualityCannotBeGreaterThan50(): void
     {
-  		return [
-  			'incr. 1 if sellIn > 10' => [11, 10, 11],
-  			'incr. 2 if 5 < sellIn <= 10 (max)' => [10, 10, 12],
-  			'incr. 2 if 5 < sellIn <= 10 (min)' => [6, 10, 12],
-  			'incr. 3 if 0 < sellIn <= 5 (max)' => [5, 10, 13],
-  			'incr. 3 if 0 < sellIn <= 5 (min)' => [1, 10, 13],
-  			'puts to 0 if sellIn <= 0 (max)' => [0, 10, 0],
-  			'puts to 0 if sellIn <= 0 (...)' => [-1, 10, 0],
-  		];
-  	}
+        $items = [new Item('Aged Brie', 0, 50)];
+
+        $gilded_rose = new GildedRose($items);
+        $gilded_rose->update_quality();
+
+        $this->assertEquals(50, $items[0]->quality);
+    }
+
+    /**
+     * @test
+     */
+    public function sulfurasDoesNotChange(): void
+    {
+        $items = [new Item('Sulfuras, Hand of Ragnaros', 10, 10)];
+
+        $gilded_rose = new GildedRose($items);
+        $gilded_rose->update_quality();
+
+        $this->assertEquals(10, $items[0]->sell_in);
+        $this->assertEquals(10, $items[0]->quality);
+    }
+
+    /**
+     * @return array <string, array<int, int>>
+     */
+    public static function backstageRules(): array
+    {
+        return [
+            'incr. 1 if sellIn > 10' => [11, 10, 11],
+            'incr. 2 if 5 < sellIn <= 10 (max)' => [10, 10, 12],
+            'incr. 2 if 5 < sellIn <= 10 (min)' => [6, 10, 12],
+            'incr. 3 if 0 < sellIn <= 5 (max)' => [5, 10, 13],
+            'incr. 3 if 0 < sellIn <= 5 (min)' => [1, 10, 13],
+            'puts to 0 if sellIn <= 0 (max)' => [0, 10, 0],
+            'puts to 0 if sellIn <= 0 (...)' => [-1, 10, 0],
+        ];
+    }
 
     /**
      * @dataProvider backstageRules
      * @test
      */
-    public function backstageQualityIncreaseOverTimeWithCertainRules($sellIn, $quality, $expected)
+    public function backstageQualityIncreaseOverTimeWithCertainRules(int $sellIn, int $quality, int $expected): void
     {
-  		$items = [new Item('Backstage passes to a TAFKAL80ETC concert', $sellIn, $quality)];
+        $items = [new Item('Backstage passes to a TAFKAL80ETC concert', $sellIn, $quality)];
 
         $gilded_rose = new GildedRose($items);
         $gilded_rose->update_quality();
 
-  		$this->assertEquals($expected, $items[0]->quality);
-  	}
+        $this->assertEquals($expected, $items[0]->quality);
+    }
 }
