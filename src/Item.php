1cc707e0d59888844409ccc3426d962b0d187f42
santiagojgonzalez <santiagojgonzalezfernandez@gmail.com>
2021-09-18 15:50:02 +0200

    Refactor: Se aplica PSR-12 a GildedRose.php y Item.php
    Se tipea Item y se crean getters y funciones de modificacion de quality y sellin necesarias, por último se aplican

--- src/Item.php
+++ src/Item.php
@@ -2,24 +2,88 @@
 
 namespace Runroom\GildedRose;
 
-class Item {
+class Item
+{
 
     const AGED_BRIE = 'Aged Brie';
     const BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT = 'Backstage passes to a TAFKAL80ETC concert';
     const SULFURAS_HAND_OF_RAGNAROS = 'Sulfuras, Hand of Ragnaros';
 
-    public $name;
-    public $sell_in;
-    public $quality;
+    public string $name;
+    public int $sell_in;
+    public int $quality;
 
-    function __construct($name, $sell_in, $quality) {
+    /**
+     * @param string $name
+     * @param int $sell_in
+     * @param int $quality
+     */
+    function __construct(string $name, int $sell_in, int $quality)
+    {
         $this->name = $name;
         $this->sell_in = $sell_in;
         $this->quality = $quality;
     }
 
-    public function __toString() {
+    public function __toString()
+    {
         return "{$this->name}, {$this->sell_in}, {$this->quality}";
     }
 
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSellIn(): int
+    {
+        return $this->sell_in;
+    }
+
+    /**
+     * @return int
+     */
+    public function getQuality(): int
+    {
+        return $this->quality;
+    }
+
+    /**
+     * @param int $quality
+     */
+    public function setQuality(int $quality): void
+    {
+        $this->quality = $quality;
+    }
+
+    /**
+     * Reduce Quality in 1
+     */
+    public function reduceQuality()
+    {
+        --$this->quality;
+    }
+
+
+    /**
+     * Boost Quality in 1
+     */
+    public function boostQuality()
+    {
+        ++$this->quality;
+    }
+
+    /**
+     * Reduce SellIn in 1
+     */
+    public function reduceSellIn()
+    {
+        --$this->sell_in;
+    }
 }
